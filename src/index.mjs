89c481148a66b8338e7f8787e2cdec6cc8a4ae81
Vinícius Hoyer <contact@vhoyer.dev>
2020-11-08 15:04:47 -0300

    :sparkles: Now script auto craws the website and produces a csv file

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,72 @@
 import axios from 'axios'
+import cheerio from 'cheerio'
+import path from 'path'
+import fs from 'fs'
 
-const url = 'https://hochanh.github.io/rtk/%E5%A9%86/index.html'
+const hochanhKanjiUrl = kanji => encodeURI(`https://hochanh.github.io/rtk/${kanji}/index.html`)
 
-axios.get(url).then(console.log)
+const lastNext = hochanhKanjiUrl('')
+const fisrtChar = '一'
+
+const [
+  startAtChar = fisrtChar,
+  output = 'output.csv'
+] = process.argv.slice(2);
+
+const selector = {
+  character: 'body > div.main > h1:nth-child(1)',
+  keyword: 'body > div.main > h2:nth-child(4) > code',
+  number: 'body > div.main > h2:nth-child(4) > code',
+  story: 'body > div.main > h2:contains("Heisig story:")',
+  primitives: 'body > div.main > h2:contains("Primitive:")',
+}
+
+const kanjiCardCsvLine = (values) => {
+  const s = key => values[key] || ''
+
+  return `${s('character')}	${s('keyword')}	${s('number')}	${s('definition')}	${s('exampleWord')}	${s('story')}	${s('primitives')}	RTK\n`
+}
+
+const KanjiCard = function($) {
+  const values = {
+    character: $(selector.character).text().trim(),
+    keyword: $(selector.keyword).text().trim(),
+    number: $(selector.number).attr('title').replace(/.*V6:\s/, '').trim(),
+    story: $(selector.story).nextUntil('h2').text(),
+    primitives: $(selector.primitives).nextUntil('h2').text(),
+  }
+
+  return {
+    values,
+    toString: () => kanjiCardCsvLine(values),
+  }
+}
+
+function go(currentUrl) {
+  return axios.get(currentUrl).then((response) => {
+    const $ = cheerio.load(response.data);
+
+    const kanji = new KanjiCard($)
+    const nextUrl = encodeURI(
+      path
+        .join(currentUrl, '..', $('a:contains("→")').attr('href'))
+        .replace('https:/', 'https://')
+    )
+
+    fs.appendFileSync(output, kanji.toString())
+
+    return nextUrl
+  })
+}
+
+fs.writeFileSync(output, '', console.error)
+
+async function run() {
+  let nextUrl = hochanhKanjiUrl(startAtChar)
+
+  while(nextUrl !== lastNext) {
+    nextUrl = await go(nextUrl)
+  }
+}
+
+run()
